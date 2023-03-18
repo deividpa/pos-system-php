f0382a118ba1567bb7d90f05431f2a7ca19e8a6a
David Pérez <deividpa022@gmail.com>
2023-03-18 12:02:30 -0500

    Se crea un wraper independiente para separar el contenido de usuarios que iniciaron sesión, de aquel que aún no lo han hecho

--- views/template.php
+++ views/template.php
@@ -14,50 +14,14 @@
 </head>
 <body class="hold-transition sidebar-collapse sidebar-mini">
 <!-- Site wrapper -->
-<div class="wrapper">
-  <!--  Heeader  -->
-  <?php
-    include "moduls/header.php";
-  ?>
-  <!--  END Header -->
-  <!-- Left Aside -->
-  <?php
-    include "moduls/asideleft.php";
-  ?>
-  <!-- END Left Aside -->
-  <!-- Content -->
-  <?php
-    
-    if(isset($_GET['path'])) {
-      if($_GET['path']=="main" ||
-          $_GET['path']=="users" ||
-            $_GET['path']=="categories" ||
-              $_GET['path']=="products" ||
-                $_GET['path']=="clients" ||
-                  $_GET['path']=="sales" ||
-                    $_GET['path']=="create-sales" ||
-                      $_GET['path']=="manage-sales" ||
-                        $_GET['path']=="view-sales" || 
-                          $_GET['path']=="home") 
-      {
-        include "moduls/".$_GET['path'].".php";
-      } else {
-        include "moduls/404.php";
-      }
-    } else {
-      include "moduls/home.php";
-    }
-    
-  ?>
-  <!-- END Content -->
-  <!-- Footer -->
-  <?php
-    include "moduls/footer.php";
-  ?>
-  <!-- END Footer -->
-</div>
-
-
+<?php
+  // When the user is loggin on 
+  if(isset($_SESSION['startSession']) && ($_SESSION['startSession'] === "ok")) {
+    include("moduls/wraper.php");
+  } else {
+    include("moduls/login.php");
+  }
+?>
 <!-- ./wrapper -->
 
 <!-- jQuery -->
