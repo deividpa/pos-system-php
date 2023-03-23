eb8f33ace6a50eb99ea69f81bfef4438675192ec
David Pérez <deividpa022@gmail.com>
2023-03-23 16:58:52 -0500

    Se finaliza implementación del modal, agregando el formulario y todos sus inputs para la creación de un nuevo usuario

--- views/moduls/users.php
+++ views/moduls/users.php
@@ -22,7 +22,6 @@
     <!-- Default box -->
     <div class="card">
       <div class="card-header">
-        <h3 class="card-title">Manage Users</h3>
         <div class="card-tools">
           <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
             <i class="fas fa-times"></i>
@@ -30,8 +29,8 @@
         </div>
         <br />
         <div class="d-block"> 
-          <button class="btn btn-primary" data-toggle="modal" data-target="#modalAddUser">
-            Add new user
+          <button class="btn btn-primary" data-toggle="modal" data-target="#modalAddUser" style="background: #6610f2">
+            Agregar Nuevo Usuario
           </button>
         </div> 
       </div>
@@ -81,21 +80,68 @@
 <!-- Creación de modal para agregar nuevo usuario -->
 <div class="modal fade" id="modalAddUser">
   <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h4 class="modal-title">Agregar Usuario</h4>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
+    <form method="post" enctype="multipart/form-data">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h4 class="modal-title">Agregar Usuario</h4>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <div class="row mb-3">
+            <label class="sr-only" for="newName">Nombre:</label>
+            <div class="input-group m-2">
+              <div class="input-group-prepend">
+                <div class="input-group-text"><i class="far fa-keyboard"></i></div>
+              </div>
+              <input type="text" class="form-control" id="newUsername" placeholder="Nombre">
+            </div>
+          </div>
+          <div class="row mb-3">
+            <label class="sr-only" for="newUsername">Usuario:</label>
+            <div class="input-group m-2">
+              <div class="input-group-prepend">
+                <div class="input-group-text"><i class="fa fa-user"></i></div>
+              </div>
+              <input type="text" class="form-control" id="newUsername" placeholder="Usuario">
+            </div>
+          </div>
+          <div class="row mb-3">
+            <label class="sr-only" for="newPassword">Contraseña</label>
+            <div class="input-group m-2">
+              <div class="input-group-prepend">
+                <div class="input-group-text"><i class="fa fa-key"></i></div>
+              </div>
+              <input type="password" class="form-control" id="newPassword" placeholder="Contraseña">
+            </div>
+          </div>
+          <div class="col-auto my-1">
+            <label class="mr-sm-2" for="newProfile">Perfil:</label>
+            <select class="custom-select mr-sm-2" id="newProfile">
+              <option selected>Seleccionar...</option>
+              <option value="seller">Vendedor</option>
+              <option value="admin">Administrador</option>
+              <option value="special">Especial</option>
+            </select>
+          </div>
+          <div class="custom-file">
+            <label class="custom-file-label mx-2" for="newPhoto">Adjuntar Foto</label>
+            <input type="file" class="custom-file-input mr-sm-2" id="newPhoto" lang="es">
+          </div>
+          <div class="d-block">
+            <img src="views/img/users/default/avatarAnon.png" 
+                  alt="Profile picture" 
+                  class="img-fluid rounded mx-auto d-block mt-3"
+                  style="max-width:30%;"  />
+          </div>
+        </div>
+        <div class="modal-footer justify-content-between">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+          <button type="submit" class="btn btn-success">Guardar Cambios</button>
+        </div>
       </div>
-      <div class="modal-body">
-        <p>Cuerpo de Página</p>
-      </div>
-      <div class="modal-footer justify-content-between">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
-        <button type="button" class="btn btn-secondary">Guardar Cambios</button>
-      </div>
-    </div>
+    </form>
     <!-- /.modal-content -->
   </div>
   <!-- /.modal-dialog -->
