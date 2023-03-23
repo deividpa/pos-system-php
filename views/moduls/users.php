dae636db271b44daa5ac96a2a2ed233a1d0c016c
David Pérez <deividpa022@gmail.com>
2023-03-23 15:15:21 -0500

    Se actualiza vista users agregando un modal para agregar nuevo usuario

--- views/moduls/users.php
+++ views/moduls/users.php
@@ -72,13 +72,32 @@
         </table>
       </div>
       <!-- /.card-body -->
-      <div class="card-footer">
-        Footer
-      </div>
-      <!-- /.card-footer-->
     </div>
     <!-- /.card -->
 
   </section>
   <!-- /.content -->
 </div>
+<!-- Creación de modal para agregar nuevo usuario -->
+<div class="modal fade" id="modalAddUser">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h4 class="modal-title">Agregar Usuario</h4>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <p>Cuerpo de Página</p>
+      </div>
+      <div class="modal-footer justify-content-between">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+        <button type="button" class="btn btn-secondary">Guardar Cambios</button>
+      </div>
+    </div>
+    <!-- /.modal-content -->
+  </div>
+  <!-- /.modal-dialog -->
+</div>
+<!-- /.modal -->
